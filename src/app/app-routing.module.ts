029610831002f2f6e3c77c9a02de39c903d615e2
KentSteinwall <kentsteinwall@gmail.com>
2020-02-12 18:25:12 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes ,RouterModule} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '', loadChildren: ()=>  import('./views/about/about.module').then(m => m.AboutModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/about/about.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes:Routes = [
+{ path: '' , component:AboutComponent }
+
+];
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class AboutModule { }
